66712bd4154384ba7b73bb5066d40f1cd39e36ce
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-01-31 09:42:13 +0300

    add more higher array methods

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const numbers = [23,44,22,88,123,777,54,98];
+const numbers = [-1,-5,-32,0,23,44,22,88,123,777,54,98];
 
 // Sort Array from lowest to largest
 const sortAscendingArray = arr=> arr.sort(function(a,b){
@@ -17,3 +17,28 @@ console.log(sortDescendingArray(numbers))
 const arrTotal = arr=> arr.reduce((num,currNum)=> num + currNum,0)
 
 console.log(arrTotal(numbers))
+
+//Filter out unwanted values in an array (Negative numbers)
+const arrFilter = arr => arr.filter(num=>num >= 0);
+console.log(arrFilter(numbers))
+
+// LOOP AN ARRAY 
+//Using forEach method
+const arrLoop = arr => arr.forEach(function(num){
+   return console.log(num)
+});
+console.log(arrLoop(numbers))
+
+//using for...of method
+const arrLoop2 = arr =>{
+    for(let num of arr){
+       console.log(num)
+    }
+}
+
+console.log(arrLoop2(numbers))
+
+// Make a copy of an array without altering the original array
+const makeCopy = arr => arr.map(num => num + 1)
+console.log(makeCopy(numbers))
+
